254790061f7dbcc1d55a91455e466d6ca157fc51
tjrapodile <47654804+tjrapodile@users.noreply.github.com>
2024-06-24 13:57:32 +0200

    Final commit

--- AbilityAssistWebApp/admin.py
+++ AbilityAssistWebApp/admin.py
@@ -3,7 +3,67 @@ from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import Trip, FinalGeolocation, InitialGeolocation, AboutImage, LocationUpdate
 import csv
+from reportlab.pdfgen import canvas
+from docx import Document
+from io import BytesIO
 from django.http import HttpResponse
+from django.utils.translation import gettext_lazy as _
+from django.db.models import Q
+
+
+class DistanceFilter(admin.SimpleListFilter):
+    title = _('distance')
+    parameter_name = 'distance'
+
+    def lookups(self, request, model_admin):
+        # This provides the options you see in the admin.
+        options = [('0km', _('0 km'))]
+        for i in range(1, 31):
+            options.append((f'{i / 10}km', _(f'{i / 10} km')))
+        options.append(('3.0km+', _('out of range trips')))
+        return options
+
+    def queryset(self, request, queryset):
+        # This filters the queryset based on the selected option.
+        if self.value() == '3.0km+':
+            return queryset.filter(distance__gt=3.0).order_by('-distance')
+        elif self.value():
+            value_km = float(self.value().replace('km', ''))
+            if value_km < 0.1:
+                # Convert kilometers to meters and filter
+                value_m = int(value_km * 1000)
+                return queryset.filter(distance__lte=f'{value_m} m').order_by('-distance')
+            else:
+                return queryset.filter(distance__lte=value_km).order_by('-distance')
+        else:
+            return queryset.order_by('-distance')
+
+class DurationFilter(admin.SimpleListFilter):
+    title = _('duration')
+    parameter_name = 'duration'
+
+    def lookups(self, request, model_admin):
+        # This provides the options you see in the admin.
+        options = [('1min', _('1 min'))]
+        for i in range(2, 21):
+            options.append((f'{i}min', _(f'{i} mins')))
+        options.append(('20min+', _('out of bound trips')))
+        return options
+
+    def queryset(self, request, queryset):
+        # This filters the queryset based on the selected option.
+        if self.value() == '20min+':
+            query = Q()
+            for i in range(21, 1000):  # Assuming a reasonable upper limit for duration
+                query |= Q(duration=f"{i} mins")  # Look for exact string match
+            return queryset.filter(query).order_by('-duration')
+        elif self.value():
+            duration_in_min = self.value().replace('min', '').strip()
+            query = Q(duration__exact="1 min")  # Include the 1-minute trips
+            for i in range(2, int(duration_in_min) + 1):
+                query |= Q(duration=f"{i} mins")  # Look for exact string match
+            return queryset.filter(query).order_by('-duration')
+        return queryset.order_by('-duration')
 
 # Define the export to CSV action
 def export_to_csv(modeladmin, request, queryset):
@@ -21,22 +81,87 @@ def export_to_csv(modeladmin, request, queryset):
 
 export_to_csv.short_description = "Export Selected to CSV"
 
+def export_to_pdf(modeladmin, request, queryset):
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="selected_data.pdf"'
+    p = canvas.Canvas(response)
+
+    y_pos = 800
+    for obj in queryset:
+        p.drawString(100, y_pos, str(obj))
+        y_pos -= 20
+        if y_pos < 100:
+            p.showPage()
+            y_pos = 800
+
+    p.showPage()
+    p.save()
+    return response
+export_to_pdf.short_description = "Export Selected to PDF"
+
+def export_to_docx(modeladmin, request, queryset):
+    response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+    response['Content-Disposition'] = 'attachment; filename="selected_data.docx"'
+    doc = Document()
+    doc.add_heading('Selected Data', 0)
+
+    for obj in queryset:
+        doc.add_paragraph(str(obj))
+
+    docx_stream = BytesIO()
+    doc.save(docx_stream)
+    docx_stream.seek(0)
+    response.write(docx_stream.read())
+    return response
+export_to_docx.short_description = "Export Selected to DOCX"
+
+def export_to_txt(modeladmin, request, queryset):
+    response = HttpResponse(content_type='text/plain')
+    response['Content-Disposition'] = 'attachment; filename="selected_data.txt"'
+    lines = [str(obj) + "\n" for obj in queryset]
+    response.writelines(lines)
+    return response
+export_to_txt.short_description = "Export Selected to TXT"
+
 # Extend the BaseUserAdmin class to include the export action
 class UserAdmin(BaseUserAdmin):
-    actions = [export_to_csv]
+    actions = [export_to_csv, export_to_pdf, export_to_docx, export_to_txt]
+    search_fields = ['username', 'first_name', 'last_name', 'email']
+
+    def mobile_numbers(self, obj):
+        return obj.username
+
+    mobile_numbers.short_description = 'Mobile Numbers'
+
+    list_display = ('mobile_numbers', 'first_name', 'last_name', 'email', 'is_staff')
 
 # ModelAdmin classes for your models
 class TripAdmin(admin.ModelAdmin):
     list_display = ('user', 'start_point', 'end_point', 'date', 'distance', 'duration', 'cancelled', 'completed') # Fields to display in list view
-    actions = [export_to_csv]
+    actions = [export_to_csv, export_to_pdf, export_to_docx, export_to_txt]
+    search_fields = ['user__username']
+    list_filter = ['cancelled', 'completed', 'date', DistanceFilter, DurationFilter]
 
 class FinalGeolocationAdmin(admin.ModelAdmin):
     list_display = ('value', 'name')  # Fields to display in list view
-    actions = [export_to_csv]
+    actions = [export_to_csv, export_to_pdf, export_to_docx, export_to_txt]
+    search_fields = ['value', 'name']
 
 class InitialGeolocationAdmin(admin.ModelAdmin):
     list_display = ('longitude', 'latitude', 'updated_at')  # Fields to display in list view
-    actions = [export_to_csv]
+    actions = [export_to_csv, export_to_pdf, export_to_docx, export_to_txt]
+    search_fields = ['longitude', 'latitude']
+
+class LocationUpdateAdmin(admin.ModelAdmin):  # Define a new ModelAdmin class for LocationUpdate
+    list_display = ('trip', 'latitude', 'longitude', 'timestamp')
+    search_fields = ['trip__user__username']
+    actions = [export_to_csv, export_to_pdf, export_to_docx, export_to_txt]
+
+class AboutImageAdmin(admin.ModelAdmin):  # Define a new ModelAdmin class for AboutImage
+    list_display = ('title', 'content')
+    search_fields = ['title', 'content']
+    actions = [export_to_csv, export_to_pdf, export_to_docx, export_to_txt]
+
 
 # Unregister the original UserAdmin and register the new UserAdmin with export action
 admin.site.unregister(User)
@@ -46,5 +171,5 @@ admin.site.register(User, UserAdmin)
 admin.site.register(Trip, TripAdmin)
 admin.site.register(FinalGeolocation, FinalGeolocationAdmin)
 admin.site.register(InitialGeolocation, InitialGeolocationAdmin)
-admin.site.register(AboutImage)
-admin.site.register(LocationUpdate)
+admin.site.register(AboutImage, AboutImageAdmin)
+admin.site.register(LocationUpdate, LocationUpdateAdmin)
--- AbilityAssistWebApp/forms.py
+++ AbilityAssistWebApp/forms.py
@@ -4,13 +4,14 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
+import requests
 from django.core.validators import validate_email, RegexValidator
 from django.core.exceptions import ValidationError
 
 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
-    username = forms.CharField(max_length=15, help_text='Required. Create your username.')
+    username = forms.CharField(max_length=10,validators=[RegexValidator(regex=r'^\d{10}$', message="Enter a valid 10-digit phone number.")], help_text='Required. Enter your phone number.')
     first_name = forms.CharField(max_length=30, help_text='Required. Enter your first name.')
     last_name = forms.CharField(max_length=30, help_text='Required. Enter your last name.')
     password1 = forms.CharField(widget=forms.PasswordInput, label='Password')
@@ -21,9 +22,31 @@ class RegistrationForm(UserCreationForm):
         fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
 
     def clean_username(self):
-        username = self.cleaned_data.get("username")
+        username = self.cleaned_data.get('username')
+        debug_messages = []
         if User.objects.filter(username=username).exists():
-            raise forms.ValidationError("This username is already in use.")
+            debug_messages.append("Phone number already in use.")
+            raise forms.ValidationError("This phone number is already in use.")
+        if not re.match(r'^(06|07|08)\d{8}$', username):
+            debug_messages.append("Invalid phone number pattern.")
+            raise forms.ValidationError("Enter a valid South African 10-digit phone number.")
+
+        api_key = '3cef86b5210bb14cbcca6382cebc6532'
+        api_url = f'http://apilayer.net/api/validate?access_key={api_key}&number={username}&country_code=ZA&format=1'
+        try:
+            response = requests.get(api_url)
+            debug_messages.append(f"API response status code: {response.status_code}")
+            response.raise_for_status()
+            data = response.json()
+            debug_messages.append(f"API response data: {data}")
+
+            if not data.get('valid', False) or data.get('line_type') != 'mobile':
+                debug_messages.append("Phone number not valid.")
+                raise forms.ValidationError("The phone number is not valid or not registered as a mobile number.")
+        except requests.RequestException:
+            debug_messages.append(f"RequestException occurred")
+            raise forms.ValidationError("Failed to validate phone number. Please try again later.")
+        print("\n".join(debug_messages))  # Print debug messages to the console
         return username
 
     def clean_first_name(self):
@@ -70,11 +93,58 @@ class LoginForm(forms.Form):
     username = forms.CharField(max_length=50)
     password = forms.CharField(widget=forms.PasswordInput)
 
+    def clean_username(self):
+        username = self.cleaned_data.get("username")
+        if not User.objects.filter(username=username).exists():
+            raise forms.ValidationError("This phone number does not exist.")
+        return username
+
 
 class EditUserForm(forms.ModelForm):
+
+    username = forms.CharField(max_length=10, required=False, validators=[
+        RegexValidator(regex=r'^\d{10}$', message="Enter a valid 10-digit phone number.")],
+        help_text='Optional. Enter a new phone number if you wish to change it.')
+
     class Meta:
         model = get_user_model()
-        fields = ['email', 'last_name']
+        fields = ['email', 'last_name', 'username']
+
+    def __init__(self, *args, **kwargs):
+        super(EditUserForm, self).__init__(*args, **kwargs)
+        self.fields['username'].initial = self.instance.username
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        debug_messages = []
+        if username and username != self.instance.username:
+            if User.objects.filter(username=username).exists():
+                debug_messages.append("Phone number already in use.")
+                raise forms.ValidationError("This phone number is already in use.")
+            if not re.match(r'^(06|07|08)\d{8}$', username):
+                debug_messages.append("Invalid phone number pattern.")
+                raise forms.ValidationError("Enter a valid South African 10-digit phone number.")
+
+            api_key = '3cef86b5210bb14cbcca6382cebc6532'
+            api_url = f'http://apilayer.net/api/validate?access_key={api_key}&number={username}&country_code=ZA&format=1'
+            try:
+                response = requests.get(api_url)
+                debug_messages.append(f"API response status code: {response.status_code}")
+                response.raise_for_status()
+                data = response.json()
+                debug_messages.append(f"API response data: {data}")
+
+                if not data.get('valid', False) or data.get('line_type') != 'mobile':
+                    debug_messages.append("Phone number not valid.")
+                    raise forms.ValidationError("The phone number is not valid or not registered as a mobile number.")
+            except requests.RequestException:
+                debug_messages.append(f"RequestException occurred")
+                raise forms.ValidationError("Failed to validate phone number. Please try again later.")
+            print("\n".join(debug_messages))  # Print debug messages to the console
+        else:
+            return self.instance.username
+        return username
+
 
     def clean_last_name(self):
         last_name = self.cleaned_data.get('last_name')
@@ -83,13 +153,14 @@ class EditUserForm(forms.ModelForm):
         return last_name
 
     def save(self, commit=True, request=None, user=None):
-        user = User.objects.get_by_natural_key(user.username)
+        user = user = self.instance
         print("Request:", request)
         print("User ID:", user.id)
         print("User username:", user.username)
         cleaned_email = self.cleaned_data.get('email')
         cleaned_last_name = self.cleaned_data.get('last_name')
-
+        cleaned_username = self.cleaned_data.get('username')
+        print("Cleaned username:", cleaned_username)  # Debugging message
         print("Cleaned email:", cleaned_email)  # Debugging message
         print("Cleaned last name:", cleaned_last_name)  # Debugging message
 
@@ -105,11 +176,20 @@ class EditUserForm(forms.ModelForm):
         if cleaned_last_name and cleaned_last_name.strip() != '':
             print("Updating last name...")  # Debugging message
             print("Existing last_name:", user.last_name)  # Add this line
-            print("Updated last_name:", user.last_name)  # Add this line
             user.last_name = cleaned_last_name
+            print("Updated last_name:", user.last_name)  # Add this line
         else:
             print("Existing last_name:", user.last_name)
             print("Last name not updated, retaining existing value.")  # Debugging message
+
+        if cleaned_username and cleaned_username.strip() != '':
+            print("Updating username...")  # Debugging message
+            print("Existing username:", user.username)  # Add this line
+            print("Updated username:", user.username)  # Add this line
+            user.username = cleaned_username
+        else:
+            print("Existing username:", user.username)
+            print("Mobile Number not updated, retaining existing value.")  # Debugging message
 
         if commit:
             print("Saving user...")  # Debugging message
